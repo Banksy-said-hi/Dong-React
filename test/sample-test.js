a33820adcd42a6109a70d6b4de0d4d480ae4bf87
Sina <sinarahimi.main@example.com>
2022-08-08 19:58:47 +0430

    Remaining commits

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { BigNumber } = require("ethers");
+// const { basic_dong } = require("../artifacts");
 
-describe("basic_dong", function () {
-  it("Calculates the dong correctly", async function () {
-    const [user1, user2, user3] = await ethers.getSigners();
-
-    const Dong = await ethers.getContractFactory("basic_dong");
-    const dong = await Dong.deploy("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", 12, 4, "Kami");
-    await dong.deployed();
-
-    const number = BigNumber.from("3000000000000000000");
-
-    await dong.payDong("sina", {value: number});
-    expect(await dong.payment(user1)).to.equal(BigNumber.from(3000000000000000000));
-  });
+describe("Dong", async () => {
+  beforeEach(async() => {
+    const accounts = await ethers.getSigners();
+    console.log(accounts[0]);
+  })
 });

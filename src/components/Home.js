acbb7aa313b8b9b2de89088e61a917d9024ee944
Sina <sinarahimi.main@example.com>
2022-08-14 14:14:48 +0430

    Test commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div className="App-home">
+            <p>Showing this means it is working finr</p>
             <Link to="./creation"><button className="button">Create new contract</button></Link>
             <h1>DONG</h1>
             <Link to="./payment"><button className="button">Pay your share</button></Link>

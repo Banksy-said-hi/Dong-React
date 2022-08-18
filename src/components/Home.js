2afa945cb7577dfaa52840837d7bf8395b0e90fd
Sina <sinarahimi.main@example.com>
2022-08-19 01:12:34 +0430

    Home-cards added for mobile compatibility

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,16 +10,32 @@ import group from "../images/group.png";
 function Home() {
 
     return (
-        <div>
-            <Header></Header>
-            <div className="home-container">
-                {/* <label>Admin</label> */}
-                <Link to="./creation"><img className="home-image" src={admin}></img></Link>
-                {/* <label>Group Members</label> */}
-                <Link to="./payment"><img className="home-image" src={group}></img></Link>
-            </div>
+        <div className="App">
+            <Link to="./creation">
+                <div className="home-card">
+                    <h1>Contact Creation</h1>
+                    <h3>Admin</h3>
+                    <img className="home-image" src={admin}></img>
+                </div>
+            </Link>
+
+            <Link to="./payment">
+                <div className="home-card">
+                    <h1>Payment</h1>
+                    <h3>Members</h3>
+                    <img className="home-image" src={admin}></img>
+                </div>
+            </Link>
         </div>
     );
 }
   
 export default Home;
+
+
+
+            // <div className="home-container">
+            //     {/* <label>Admin</label> */}
+            //     {/* <label>Group Members</label> */}
+            //     <Link to="./payment"><img className="home-image" src={group}></img></Link>
+            // <
